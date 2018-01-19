f9f0008343fc6836a9e9794336ffbf353d436ce0
Adrian Ivanciu <adrianivanciu@yahoo.com>
2018-01-19 11:37:03 +0000

    am scos butonul de inregistrare

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,7 @@
                     <!-- Authentication Links -->
                     @if (Auth::guest())
                         <li><a href="{{ url('/login') }}">Autentificare</a></li>
-                        <li><a href="{{ url('/register') }}">Înregistrare</a></li>
+{{--                        <li><a href="{{ url('/register') }}">Înregistrare</a></li>--}}
                     @else
                         <li class="dropdown">
                             {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">--}}
